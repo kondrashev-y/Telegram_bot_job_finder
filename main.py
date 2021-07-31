ac35b0bdf30c1e947ee1d6afb7a5b034df48a428
kondrashev-y <70172501+kondrashev-y@users.noreply.github.com>
2021-07-31 22:00:01 +0300

    add telebot

--- main.py
+++ main.py
@@ -1,136 +1,144 @@
-import re
-
 import requests
-from json.decoder import JSONDecodeError
 from flask import Flask
-from flask.views import MethodView
 from flask import request, Response
+import telebot
 import os
 
 
 app = Flask(__name__)
 TOKEN = os.environ.get('TOKEN')
 API_URL = os.environ.get('API_URL')
-TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+bot = telebot.TeleBot(TOKEN)
+BOT_URL = os.environ.get('BOT_URL')
+
+
+choice_lang = ''
+choice_city = ''
 
 
 def get_data_from_api(command):
     url = API_URL + command
-    print(url, 'this is URL+command')
-    session = requests.Session()
-    r = session.get(url).json()
-    print(r, 'this is r in get_data_from_api')
-    return r
+    try:
+        session = requests.Session()
+        r = session.get(url).json()
+        return r
+    except Exception as e:
+        print(repr(e))
+        return []
 
 
-def send_message(chat_id, msg):
-    session = requests.Session()
-    r = session.get(TELEGRAM_URL, params=dict(chat_id=chat_id, text=msg, parse_mode='Markdown'))
-    return r.json()
+try:
+    lang_list = get_data_from_api('/lang')
+    lang_slug_list = [item['slug'] for item in lang_list]
+    cities_list = get_data_from_api('/cities')
+    cities_slug_list = [item['slug'] for item in cities_list]
+except Exception as e:
+    print(repr(e))
+    pass
 
 
-def parse_text(text_message):
-    """Парсер входящих сообщений"""
-    addresses = {'city': '/cities', 'lang': '/lang'}
-    command_p = r'/\w+'
-    doc_pattern = r'@\w+'
-    message = 'Неверный запрос'
-    if '/' in text_message:
-        if '/start' in text_message or '/help' in text_message:
-            message = '''Для того, чтобы узнать, какие города доступны, отправь в сообщени `/city`.
-            Чтобы узнать о доступных языках программирования - отправь `/lang`.
-            Чтобы сделать запрос на вакансии, отправь в сообщении через пробел  - @город @язык.
-            Например так - `@moscow @python` '''
-            return message
-        else:
-            command = re.search(command_p, text_message).group().replace('/', '')
-            command = addresses.get(command, None)
-            return [command] if command else None
-    elif '@' in text_message:
-        result = re.findall(doc_pattern, text_message)
-        commands = [s.replace('@', '') for s in result]
-        return commands if len(commands) == 2 else None
-    else:
-        return message
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup_2 = telebot.types.InlineKeyboardMarkup()
+    button_lang = telebot.types.InlineKeyboardButton('Начать поиск', callback_data='lang')
+    markup_2.row(button_lang)
+    button_a = telebot.types.InlineKeyboardButton('/help')
+    markup.row(button_a)
+    bot.send_message(message.chat.id, 'Привет, это бот сервиса по сбору вакансий, '
+                                      'введу команду /help для подробной информации.', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Или сразу переходи к поиску', reply_markup=markup_2)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@bot.message_handler(commands=['help'])
+def hel_info(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+
+    button_lang = telebot.types.InlineKeyboardButton('Начать поиск', callback_data='lang')
+
+    markup.row(button_lang)
+
+    bot.send_message(message.chat.id, 'Привет, это телеграмм бот сервиса по сбору вакансий, для отображения вакансий'
+                                      'за сегоднешний деннь необходимо выбрать язык и город.', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ['lang'])
+def lang(call):
+    global lang_list
+    markup = telebot.types.InlineKeyboardMarkup(row_width=2)
+    buttons = []
+    for item in lang_list:
+        buttons.append(telebot.types.InlineKeyboardButton(text=item['name'], callback_data=item['slug']))
+    markup.add(*buttons)
+    bot.send_message(call.message.chat.id, 'Выберите город', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in lang_slug_list + cities_slug_list)
+def callback_inline(call):
+    global choice_lang, choice_city
+
+    if call.data in lang_slug_list:
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.id)
+
+        markup = telebot.types.InlineKeyboardMarkup()
+        buttons = []
+
+        for city in cities_list:
+            buttons.append(markup.add(telebot.types.InlineKeyboardButton(text=city['name'],
+                                                                         callback_data=city['slug'])))
+        bot.send_message(call.message.chat.id, 'Выберите город', reply_markup=markup)
+        choice_lang = call.data
+
+    elif call.data in cities_slug_list:
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.id)
+        choice_city = call.data
+        bot.send_message(call.message.chat.id, 'Подождите, загружаю...')
+        try:
+            if choice_city and choice_lang:
+                command = f'/vacancy/?city={choice_city}&lang={choice_lang}'
+                if '#' in choice_lang:
+                    choice_lang = 'c%23'
+                vacancy = get_data_from_api(command)
+                markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+                buttons = []
+                if len(vacancy) == 0:
+                    bot.send_message(call.message.chat.id, 'Упс, на сегодня вакансий еще '
+                                                           'не появилось, попробуйте позже')
+                for item in vacancy:
+                    buttons.append(telebot.types.InlineKeyboardButton(text=f"{item['title']} - {item['salary']}",
+                                                                      url=item['url']))
+                    if len(buttons) % 10 == 0:
+                        markup.add(*buttons)
+                        bot.send_message(call.message.chat.id, 'Доступные вакансии', reply_markup=markup)
+                        markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+                        buttons = []
+                if buttons:
+                    markup.add(*buttons)
+                    bot.send_message(call.message.chat.id, 'Доступные вакансии', reply_markup=markup)
+            else:
+                bot.send_message(call.chat.id, 'Что то пошло не  так, выберите язык и город')
+        except Exception as e:
+            print(repr(e))
+
+
+@bot.message_handler(func=lambda message: True)
+def other_message(message):
+    bot.send_message(message.chat.id, 'Упс, неверная команда, попробуйте еще раз.')
+
+
+@app.route("/" + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        return '<h1>Hi bot!</h1>'
     return '<h1>Hi bot!</h1>'
 
 
-class BotAPI(MethodView):
-    """Оправление сообщений клиенту"""
-
-    def get(self):
-        return '<h1>Hi bot_class!</h1>'
-
-    def post(self):
-        text_error = 'Неверный запрос'
-        text_server_error = 'Сервер не отвечает'
-        try:
-            resp = request.get_json()
-            text_massage = resp['message']['text']
-            chat_id = resp['message']['chat']['id']
-            print(text_massage, 'this text_message in BotAPI')
-            tmp = parse_text(text_massage)
-            if tmp:
-                if len(tmp) > 10:
-                    send_message(chat_id, tmp)
-                elif len(tmp) == 1:
-                    message = ''
-                    msg = ''
-                    resp = get_data_from_api(tmp[0])
-                    if resp:
-                        for d in resp:
-                            message += '`' + d['slug'] + '`' + '\n'
-                        if tmp[0] == '/lang':
-                            msg = "Доступные языки: \n"
-                        else:
-                            msg = "Доступные города: \n"
-                    send_message(chat_id, msg + message)
-                elif len(tmp) == 2:
-                    url = '/vacancy/?city={}&lang={}'.format(*tmp)
-                    resp = get_data_from_api(url)
-                    if resp:
-                        parts = []
-                        size = len(resp)
-                        extra = size % 10
-                        if size < 11:
-                            parts.append(resp)
-                        else:
-                            num = 0
-                            for i in range(size//10):
-                                num = i * 10
-                                parts.append(resp[num:num+10])
-                            if extra:
-                                parts.append(resp[num+10:])
-                        text_msg = 'Рузльтат поиска согласно вашему запросу:\n' + '- ' * 10
-                        send_message(chat_id, text_msg)
-
-                        for part in parts:
-                            message = ''
-                            for item in part:
-                                message += item['title'] + '\n'
-                                message += item['url'] + '\n'
-                                message += item['company'] + ' - '
-                                message += item['salary'] + '\n'
-                                message += '-' * 5 + '\n\n'
-                            send_message(chat_id, message)
-                    else:
-                        send_message(chat_id, text_server_error)
-
-            else:
-                send_message(chat_id, text_error)
-            return '<h1>Hi i am chat bot!</h1>'
-        except JSONDecodeError:
-            print('json error')
-            return Response(status='200')
-
-
-app.add_url_rule(f'/{TOKEN}/', view_func=BotAPI.as_view('bot'))
+bot.remove_webhook()
+bot.set_webhook(BOT_URL + TOKEN)
 
 if __name__ == '__main__':
     app.run()
